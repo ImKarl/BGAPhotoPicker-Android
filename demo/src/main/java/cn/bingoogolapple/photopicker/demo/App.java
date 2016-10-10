7945ffa2ed88ddd4c04226927801868b7951effd
imkarl <582086295@qq.com>
2016-10-10 17:30:18 +0800

    升级tool版本；使用gradle方式引用PhotoView

--- demo/src/main/java/cn/bingoogolapple/photopicker/demo/App.java
+++ demo/src/main/java/cn/bingoogolapple/photopicker/demo/App.java
@@ -2,8 +2,6 @@ package cn.bingoogolapple.photopicker.demo;
 
 import android.app.Application;
 
-import com.squareup.leakcanary.LeakCanary;
-
 /**
  * 作者:王浩 邮件:bingoogolapple@gmail.com
  * 创建时间:16/7/11 上午10:23
@@ -14,6 +12,5 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        LeakCanary.install(this);
     }
 }
